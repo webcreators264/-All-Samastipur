ca3178a4518143feb7be5075754b95b98bb4f7d5
webcreators264 <webcreators264@gmail.com>
2026-08-21 16:44:40 +0530

    Update firebase-ride.js

--- firebase-ride.js
+++ firebase-ride.js
@@ -1,3 +1,29 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+
+import {
+  getDatabase,
+  ref,
+  set,
+  get,
+  update,
+  onValue,
+  push
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCcaADTLXm-UEIjQlgsq3zntpXcZZ3h4v8",
+  authDomain: "all-samastipur.firebaseapp.com",
+  projectId: "all-samastipur",
+  storageBucket: "all-samastipur.firebasestorage.app",
+  messagingSenderId: "731332314428",
+  appId: "1:731332314428:web:fb0b4bdb9ee3cbc74a8a39",
+  measurementId: "G-13W0M5QYHJ"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
+
 export {
   app,
   database,
@@ -8,3 +34,5 @@ export {
   onValue,
   push
 };
+
+console.log("All Samastipur Firebase Connected!");
