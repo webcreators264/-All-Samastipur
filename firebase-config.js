9a3e834662ee1944c474cb936a03aea8a6635f5d
webcreators264 <webcreators264@gmail.com>
2026-08-21 16:37:32 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,84 @@
+// ==========================================
+// ALL SAMASTIPUR - FIREBASE CONFIG
+// Code 19 - Backend Connection
+// ==========================================
+
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+
+import {
+  getDatabase,
+  ref,
+  set,
+  get,
+  update,
+  onValue,
+  push
+} from
+"https://www.gstatic.com/firebasejs/12.18.0/firebase-database.js";
+
+
+// Firebase Configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCcaADTLXm-UEIjQlgsq3zntpXcZZ3h4v8",
+
+  authDomain:
+    "all-samastipur.firebaseapp.com",
+
+  projectId:
+    "all-samastipur",
+
+  storageBucket:
+    "all-samastipur.firebasestorage.app",
+
+  messagingSenderId:
+    "731332314428",
+
+  appId:
+    "1:731332314428:web:fb0b4bdb9ee3cbc74a8a39",
+
+  measurementId:
+    "G-13W0M5QYHJ"
+};
+
+
+// Initialize Firebase
+
+const app =
+  initializeApp(firebaseConfig);
+
+
+// Initialize Realtime Database
+
+const database =
+  getDatabase(app);
+
+
+// Make Firebase available to other HTML files
+
+window.AllSamastipurFirebase = {
+
+  app: app,
+
+  database: database,
+
+  ref: ref,
+
+  set: set,
+
+  get: get,
+
+  update: update,
+
+  onValue: onValue,
+
+  push: push
+
+};
+
+
+console.log(
+  "All Samastipur Firebase Connected!"
+);
